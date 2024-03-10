87e287a66af793f4add1b10fea930701030c10d7
Nasghul01 <nasrul01arsyad@gmail.com>
2024-03-10 18:44:55 +0800

    create 10 dice interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import { useState } from 'react'
+import Die from './Die'
 
 function App() {
 
   return (
     <>
-      
+      <main>
+        <div className='dice-container'>
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+        </div>
+
+
+      </main>
     </>
   )
 }
