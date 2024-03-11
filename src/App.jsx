323b9f23a8166c09862405b81b7752185031c85d
Nasghul01 <nasrul01arsyad@gmail.com>
2024-03-11 14:03:53 +0800

    add color when its held true

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,40 @@
 import { useState } from 'react'
 import Die from './Die'
+import { nanoid } from 'nanoid'
 
 function App() {
+
+  const [dice, setDice] = useState(allNewDice())
 
   function allNewDice() {
     const newDice = []
     for (let i=0 ; i < 10 ; i++){
-      newDice.push(Math.ceil(Math.random() * 6))
-      
+      newDice.push({
+        value: Math.ceil(Math.random() * 6),
+        isHeld: true,
+        id: nanoid()},
+        ) 
     }
     return newDice
   }
 
-  console.log(allNewDice())
+  function rollDice () {
+    setDice(allNewDice())
+  }
+
+  const diceElement = dice.map(die => (
+    <Die key={die.id} 
+    value={die.value} 
+    isHeld= {die.isHeld}/>
+  ))
 
   return (
     <>
       <main>
         <div className='dice-container'>
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
-          <Die value="1" />
+          {diceElement}
         </div>
-
-
+        <button onClick={rollDice} className='roll-dice'>Roll</button>
       </main>
     </>
   )
