b1190d8b52442374f3ddce5c653bc9db83f9ebb1
Nasghul01 <nasrul01arsyad@gmail.com>
2024-03-13 14:16:57 +0800

    hold the number of the dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,57 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Die from './Die'
 import { nanoid } from 'nanoid'
 
 function App() {
 
   const [dice, setDice] = useState(allNewDice())
+  const [tenzies, setTenZies] = useState(false)
+
+  useEffect(() => {
+    const allHeld = dice.every(die => die.isHeld)
+    const firstValue = dice[0].value
+    const allSameValue = dice.every(die => die.value == firstValue)
+
+    if (allHeld && allSameValue){
+      setTenZies(true)
+      console.log("you won")
+    }
+
+
+  }, [dice])
 
   function allNewDice() {
     const newDice = []
     for (let i=0 ; i < 10 ; i++){
       newDice.push({
         value: Math.ceil(Math.random() * 6),
-        isHeld: true,
+        isHeld: false,
         id: nanoid()},
         ) 
     }
     return newDice
   }
 
+  function generateNewDie () {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid()}
+    }
+
   function rollDice () {
-    setDice(allNewDice())
+    setDice(oldDice => oldDice.map(die => {
+      return die.isHeld ?
+      die : generateNewDie()
+    }))
   }
 
+
   function holdDice (id) {
-    console.log(id)
+    setDice(oldDice => oldDice.map( die => {
+      return die.id === id ? 
+      {...die, isHeld: !die.isHeld} : die
+    }))
   }
 
   const diceElement = dice.map(die => (
